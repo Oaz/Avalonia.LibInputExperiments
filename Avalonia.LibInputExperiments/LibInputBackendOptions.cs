fca189a668d4b5f4a8328d6c81825347fee4fde2
Tiago Conceição <Tiago_caza@hotmail.com>
2024-07-24 15:17:36 +0100

    Add option to auto pool of new devices and enable them

--- Avalonia.LibInputExperiments/LibInputBackendOptions.cs
+++ Avalonia.LibInputExperiments/LibInputBackendOptions.cs
@@ -9,6 +9,17 @@ namespace Avalonia.LibInputExperiments;
 public sealed record class LibInputBackendOptions
 {
     /// <summary>
+    /// Sets to listen for event changes, if true it will listen for events changes and register them to make the device work without restarting the application.
+    /// </summary>
+    public bool ListenEventsChanges { get; init; }
+
+    /// <summary>
+    /// Sets the listen for event changes interval in milliseconds.<br/>
+    /// Requires <see cref="ListenEventsChanges"/> to be true.
+    /// </summary>
+    public int ListenEventsChangesInterval { get; init; } = 5000;
+
+    /// <summary>
     /// List Events of events handler to monitoring eg: /dev/eventX.
     /// </summary>
     public IReadOnlyList<string>? Events { get; init; } = null;
